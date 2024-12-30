8ec050febe8e3fc9c27fd29e083816166d0d794b
Nahuel Beeguer <44383860+MexxaDev@users.noreply.github.com>
2024-12-30 13:48:08 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const nextBtn = document.getElementById('next-btn');
 
 let currentIndex = 0;
 function iniciar(){
-    alert("Hay que esperar hasta el 4/4/2025 para comenzar con este apartado ✈");
+    alert("Hay que esperar un poco mas para comenzar con este apartado ✈");
 }
 alert("Espero que podamos completar este Album digital con muchos momentos juntos❤");
 function updateMainImage(index) {
